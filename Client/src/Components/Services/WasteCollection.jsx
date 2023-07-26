90a90e97f49531c701d8caa697022817e7eeaf65
Gopal <gopalsompura111@gmail.com>
2023-07-25 19:26:12 -0500

    1st Commit

--- Client/src/Components/Services/WasteCollection.jsx
+++ Client/src/Components/Services/WasteCollection.jsx
@@ -11,6 +11,7 @@ function WasteCollection() {
   const userdetails = localStorage.getItem("user");
   const currentuser = JSON.parse(userdetails);
   const token = localStorage.getItem("token");
+  console.log(token);
 
   const [conversation, setConversation] = useState([]);
   const [messages, setMessages] = useState([]);
